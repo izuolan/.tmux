07b45512253abcd17c5ab11e3c8004a7ddb75a25
ZuoLan <i@zuolan.me>
2017-06-11 14:39:18 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TMUX_VERSION=2.4
-apt install -y libncurses5-dev libevent-dev
-wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz -O /tmp/tmux-${TMUX_VERSION}.tar.gz
-tar xf /tmp/tmux-${TMUX_VERSION}.tar.gz
-cd /tmp/tmux-${TMUX_VERSION} && ./configure && make
-make install
-rm -rf /tmp/tmux-*
